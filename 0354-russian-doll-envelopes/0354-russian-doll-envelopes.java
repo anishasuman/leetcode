63fef1462b27df9b2ce0d5ac4ee270d716d7682e
Anisha Kumari <149311097+anishasuman@users.noreply.github.com>
2026-07-24 12:26:20 +0530

    Time: 44 ms (50.37%), Space: 101.6 MB (26.81%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -1,26 +1,38 @@
- class Solution{
- public int maxEnvelopes(int[][] arr){
-        Arrays.sort(arr, (a, b) -> (a[0] != b[0]) ? Integer.compare(a[0], b[0]) : Integer.compare(b[1], a[1]));
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int[] a : arr){
-            int ele = a[1];// for 2D array
-            if(ans.size() == 0 || ele > ans.get(ans.size()-1)) ans.add(ele);
-            else replace(ele,ans);
-        }
-        return ans.size();
-    }
+class Solution {
 
-    private void replace(int ele,ArrayList<Integer> ans){
-        //Find lower bound of ele
-        int lo = 0, hi = ans.size() - 1, lb = -1;
-        while(lo <= hi){
-            int mid = lo + (hi-lo)/2;
-            if(ans.get(mid) >= ele){
-                lb = mid;
-                hi = mid - 1;
+    public int maxEnvelopes(int[][] envelopes) {
+
+        Arrays.sort(envelopes, (a, b) -> {
+            if (a[0] == b[0])
+                return b[1] - a[1];
+            return a[0] - b[0];
+        });
+
+        ArrayList<Integer> lis = new ArrayList<>();
+
+        for (int[] env : envelopes) {
+
+            int h = env[1];
+
+            int left = 0;
+            int right = lis.size();
+
+            while (left < right) {
+
+                int mid = (left + right) / 2;
+
+                if (lis.get(mid) < h)
+                    left = mid + 1;
+                else
+                    right = mid;
             }
-            else lo = mid + 1;
+
+            if (left == lis.size())
+                lis.add(h);
+            else
+                lis.set(left, h);
         }
-        ans.set(lb,ele);
+
+        return lis.size();
     }
 }
