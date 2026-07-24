ddd151eda5899ea3accdc3c852ef9493ab601d2f
Anisha Kumari <149311097+anishasuman@users.noreply.github.com>
2026-07-24 16:41:39 +0530

    Time: 4 ms (100%), Space: 45.9 MB (53.52%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,13 +1,30 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         String s = String.valueOf(x);
+//         int l = 0;
+//         int r = s.length()-1;
+//         while(l<r){
+//             if(s.charAt(l)!=s.charAt(r))return false;
+//             l++;
+//             r--;
+//         }
+//         return true;
+//     }
+// }
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        int l = 0;
-        int r = s.length()-1;
-        while(l<r){
-            if(s.charAt(l)!=s.charAt(r))return false;
-            l++;
-            r--;
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        return true;
+
+        int rev = 0;
+
+        while (x > rev) {
+            rev = rev * 10 + x %10;
+            x = x / 10;
+        }
+        if (x == rev) return true; // Even digits (1221)
+        if (x == rev / 10) return true;// Odd digits (12321)
+        return false;
     }
 }
